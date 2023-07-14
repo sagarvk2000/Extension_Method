05fc98fed36e6a7c7e5e233f08553968be4aca55
SnehalKawade <snehkawade@gmail.com>
2023-07-14 16:50:46 +0530

    Add project files.

---
+++ Extension_Method/Calculation.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Method
+{
+    public class Calculation
+    {
+        public int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+    }
+    public static class ExtensionClass
+    {
+        public static int Square(this Calculation c,int a)
+        {
+            return a * a;
+        }
+        public static int Multiply(this Calculation c, int a,int b)
+        {
+            return a * b;
+        }
+
+    }
+}
+
---
+++ Extension_Method/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Extension_Method
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculation c = new Calculation();
+            int res=c.Square(4);
+            Console.WriteLine(res);
+            int res2 = c.Multiply(5,6);
+            Console.WriteLine(res2);
+        }
+    }
+}
